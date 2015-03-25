edbbcd52e9f527ea90dc3cebf07a57eba3d3ed31
Tom Wilkinson <twilkinson@expedia.com>
2015-03-25 17:13:36 +0000

    Want output to end up in syslog and STDOUT, but want to preserve exit code, hence not using tee as a pipe to ./installer

--- bootstraps/linux.sh
+++ bootstraps/linux.sh
@@ -9,18 +9,21 @@ curl -s {{environmentConfiguration}} > ./environmentSetup
 curl -s {{installerPath}} > ./installer
 chmod 755 ./installer
 if [ ${UID} == 0 ]; then
-	./installer
+	./installer > ./pleasance-agent.log
 	exitcode=$?
 	mkdir -p /var/db/pleasance
 	[ ${exitcode} == 0 ] && echo "{{packageVersion}}" > /var/db/pleasance/{{packageName}}.version
+	cat ./pleasance-agent.log | logger -t pleasance-agent
+        cat ./pleasance-agent.log
 	cd
 	rm -rf ${workdir}
 else
-	sudo ./installer
+	sudo ./installer > ./pleasance-agent.log
 	exitcode=$?
 	sudo mkdir -p /var/db/pleasance
 	[ ${exitcode} == 0 ] && echo "{{packageVersion}}" | sudo tee /var/db/pleasance/{{packageName}}.version >/dev/null
-	cd
+	cat ./pleasance-agent.log | logger -t pleasance-agent
+	cat ./pleasance-agent.log
 	sudo rm -rf ${workdir}
 fi
 exit ${exitcode}
