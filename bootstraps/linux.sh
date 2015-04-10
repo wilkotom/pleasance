8b56e2ac230b6d70eac85b4c4169c74fb85dac0f
Tom Wilkinson <twilkinson@expedia.com>
2015-04-10 11:58:46 +0100

    Better logging for bootstrap

--- bootstraps/linux.sh
+++ bootstraps/linux.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
+if [ ${POSIXLY_CORRECT} ]; then
+	echo "ERROR: Pleasance Bootstrap must be invoked through /bin/bash, not /bin/sh" >&2
+	exit 1
+fi
+
 workdir=$(mktemp -d)
 
 cd ${workdir}
-
+echo "$(date): Pleasance Bootstrap initiated for {{packageName}} version {{packageVersion}}" | tee >(exec logger -t pleasance-agent)
 curl -s {{packageURL}} > ./packagefile
 curl -s {{environmentConfiguration}} > ./environmentSetup
 curl -s {{installerPath}} > ./installer
 chmod 755 ./installer
 if [ ${UID} == 0 ]; then
-	./installer > ./pleasance-agent.log
+	./installer | tee >(exec logger -t pleasance-agent)
 	exitcode=$?
 	mkdir -p /var/db/pleasance
 	[ ${exitcode} == 0 ] && echo "{{packageVersion}}" > /var/db/pleasance/{{packageName}}.version
-	cat ./pleasance-agent.log | logger -t pleasance-agent
-        cat ./pleasance-agent.log
 	cd
 	rm -rf ${workdir}
 else
-	sudo ./installer > ./pleasance-agent.log
+	sudo ./installer | tee >(exec logger -t pleasance-agent)
 	exitcode=$?
 	sudo mkdir -p /var/db/pleasance
 	[ ${exitcode} == 0 ] && echo "{{packageVersion}}" | sudo tee /var/db/pleasance/{{packageName}}.version >/dev/null
-	cat ./pleasance-agent.log | logger -t pleasance-agent
-	cat ./pleasance-agent.log
 	sudo rm -rf ${workdir}
 fi
 exit ${exitcode}
