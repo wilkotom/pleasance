d53c9bd1c4196bd3a607d222fb2516ba014efa1d
Tom Wilkinson <twilkinson@expedia.com>
2015-07-03 10:42:10 +0100

    Tidy up code, fix issue whereby installer status wasn't properly returned by the script

--- bootstraps/linux.sh
+++ bootstraps/linux.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ${UID} != 0 ]; then
+	sudo $0
+	exit $?
+fi
+
 if [ ${POSIXLY_CORRECT} ]; then
 	echo "ERROR: Pleasance Bootstrap must be invoked through /bin/bash, not /bin/sh" >&2
 	exit 1
@@ -13,19 +18,9 @@ curl -s {{packageURL}} > ./packagefile
 curl -s {{environmentConfiguration}} > ./environmentSetup
 curl -s {{installerPath}} > ./installer
 chmod 755 ./installer
-if [ ${UID} == 0 ]; then
-	./installer | tee >(exec logger -t pleasance-agent)
-	exitcode=$?
-	mkdir -p /var/db/pleasance
-	[ ${exitcode} == 0 ] && echo "{{packageVersion}}" > /var/db/pleasance/{{packageName}}.version
-	cd
-	rm -rf ${workdir}
-else
-	sudo ./installer | tee >(exec logger -t pleasance-agent)
-	exitcode=$?
-	sudo mkdir -p /var/db/pleasance
-	[ ${exitcode} == 0 ] && echo "{{packageVersion}}" | sudo tee /var/db/pleasance/{{packageName}}.version >/dev/null
-	sudo rm -rf ${workdir}
-fi
+./installer | tee >(exec logger -t pleasance-agent)
+exitcode=${PIPESTATUS[0]}
+sudo mkdir -p /var/db/pleasance
+[ ${exitcode} == 0 ] && echo "{{packageVersion}}" | sudo tee /var/db/pleasance/{{packageName}}.version >/dev/null
+sudo rm -rf ${workdir}
 exit ${exitcode}
-
