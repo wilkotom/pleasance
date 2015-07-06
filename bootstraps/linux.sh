9b7efde4c33c2b76c01905add7a2a3889d53a282
Tom Wilkinson <twilkinson@expedia.com>
2015-07-06 14:02:18 +0100

    Previous 'smart' approach to sudo wouldn't work in the curl https://blahblah | sudo bash situation; refactored code

--- bootstraps/linux.sh
+++ bootstraps/linux.sh
@@ -1,8 +1,10 @@
 #!/bin/bash 
 
+SUDOPREFIX=''
+
 if [ ${UID} != 0 ]; then
-	sudo $0
-	exit $?
+        # If the script isn't running as root, we need to prepend certain commands with sudo 
+	SUDOPREFIX="sudo"
 fi
 
 if [ ${POSIXLY_CORRECT} ]; then
@@ -18,9 +20,9 @@ curl -s {{packageURL}} > ./packagefile
 curl -s {{environmentConfiguration}} > ./environmentSetup
 curl -s {{installerPath}} > ./installer
 chmod 755 ./installer
-./installer | tee >(exec logger -t pleasance-agent)
+${SUDOPREFIX} ./installer | tee >(exec logger -t pleasance-agent)
 exitcode=${PIPESTATUS[0]}
-mkdir -p /var/db/pleasance
-[ ${exitcode} == 0 ] && echo "{{packageVersion}}" | sudo tee /var/db/pleasance/{{packageName}}.version >/dev/null
-rm -rf ${workdir}
+${SUDOPREFIX} mkdir -p /var/db/pleasance
+[ ${exitcode} == 0 ] && echo "{{packageVersion}}" | ${SUDOPREFIX} tee /var/db/pleasance/{{packageName}}.version >/dev/null
+${SUDOPREFIX} rm -rf ${workdir}
 exit ${exitcode}
